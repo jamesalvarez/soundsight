69a454c9f8b4f82a2261e4107166dc8e6c321b3c
James Alvarez <jamesa333uk@gmail.com>
2023-01-27 15:07:03 +0000

    Improve look of patches

--- Synestheatre/UIColor+ExtraColours.m
+++ Synestheatre/UIColor+ExtraColours.m
@@ -19,21 +19,23 @@
 
 + (UIColor *)patchColor:(int)index {
     
+    float brightness = 0.7;
+    
     switch (index) {
         case 0:
-            return [UIColor colorWithHueDegrees:0 saturation:0.8 brightness:0.9];
+            return [UIColor colorWithHueDegrees:0 saturation:0.8 brightness:brightness];
         case 1:
-            return [UIColor colorWithHueDegrees:45 saturation:0.8 brightness:0.9];
+            return [UIColor colorWithHueDegrees:45 saturation:0.8 brightness:brightness];
         case 2:
-            return [UIColor colorWithHueDegrees:90 saturation:0.8 brightness:0.9];
+            return [UIColor colorWithHueDegrees:90 saturation:0.8 brightness:brightness];
         case 3:
-            return [UIColor colorWithHueDegrees:135 saturation:0.8 brightness:0.9];
+            return [UIColor colorWithHueDegrees:135 saturation:0.8 brightness:brightness];
         case 4:
-            return [UIColor colorWithHueDegrees:180 saturation:0.8 brightness:0.9];
+            return [UIColor colorWithHueDegrees:180 saturation:0.8 brightness:brightness];
         case 5:
-            return [UIColor colorWithHueDegrees:215 saturation:0.8 brightness:0.9];
+            return [UIColor colorWithHueDegrees:215 saturation:0.8 brightness:brightness];
         default:
-            return [UIColor colorWithHueDegrees:250 saturation:0.8 brightness:0.9];
+            return [UIColor colorWithHueDegrees:250 saturation:0.8 brightness:brightness];
     }
     
 }
