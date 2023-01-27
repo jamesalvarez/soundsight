cc5b8a108a166ad86a1502fe270e2c393961a8b4
James Alvarez <jamesa333uk@gmail.com>
2023-01-27 15:04:58 +0000

    Removed unused defaults code

--- Synestheatre/AppDelegate.m
+++ Synestheatre/AppDelegate.m
@@ -24,9 +24,6 @@
         exit(0);
     }
     
-    // Set defaults
-    [self registerDefaultsFromSettingsBundle];
-    
     // Prevent dimming
     [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
     
@@ -38,42 +35,6 @@
     return YES;
 }
 
-
-- (void)registerDefaultsFromSettingsBundle
-{
-    [[NSUserDefaults standardUserDefaults] registerDefaults:[self defaultsFromPlistNamed:@"Root"]];
-}
-
-- (NSDictionary *)defaultsFromPlistNamed:(NSString *)plistName
-{
-    NSString *settingsBundle = [[NSBundle mainBundle] pathForResource:@"InAppSettings" ofType:@"bundle"];
-    NSString *plistFullName = [NSString stringWithFormat:@"%@.plist", plistName];
-    NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:[settingsBundle stringByAppendingPathComponent:plistFullName]];
-    NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
-    
-    NSMutableDictionary *defaults = [NSMutableDictionary dictionary];
-    
-    for (NSDictionary *prefSpecification in preferences)
-    {
-        NSString *key = [prefSpecification objectForKey:@"Key"];
-        id value = [prefSpecification objectForKey:@"DefaultValue"];
-        
-        if (key && value)
-        {
-            [defaults setObject:value forKey:key];
-        }
-        
-        NSString *type = [prefSpecification objectForKey:@"Type"];
-        
-        if ([type isEqualToString:@"PSChildPaneSpecifier"])
-        {
-            NSString *file = [prefSpecification objectForKey:@"File"];
-            [defaults addEntriesFromDictionary:[self defaultsFromPlistNamed:file]];
-        }
-    }
-    
-    return defaults;
-}
 
 - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
     
